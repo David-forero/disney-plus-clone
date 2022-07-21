fb7ee71da4081d9c95abcf359f937fed066da9c6
David Forero <54685136+David-forero@users.noreply.github.com>
2022-07-21 10:32:46 -0400

    code complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Header from "./components/Header";
 import "./App.css";
 import Home from "./components/Home";
-import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
 import Detail from "./components/Detail";
-import AppComponent from "./views/layouts/AppComponent";
-import Login from "./components/Login";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-      {/* Public */}
-        <Route path="/" element={<AppComponent />}>
-          <Route path="/detail" element={<Detail />} />
-          <Route index element={<Home />} />
-          <Route path="/login" element={<Login />} />
-        </Route>
-
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/detail/:id">
+            <Detail />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
